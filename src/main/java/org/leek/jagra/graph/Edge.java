175d804374f8828bb05778a8220ac3a4ce2a6a8e
Felix <felix.wedler@t-online.de>
2024-03-17 00:57:37 +0100

    Second part of directory structure change

---
+++ src/main/java/org/leek/jagra/graph/Edge.java
@@ -0,0 +1,13 @@
+package org.leek.jagra.graph;
+
+/**
+ * Interface for the edges of this graph.
+ */
+public abstract class Edge {
+  protected Vertex start;
+  protected Vertex end;
+
+  public Edge() {
+
+  }
+}
